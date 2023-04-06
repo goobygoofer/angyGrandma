64218ddf7a4bd03f7036da0b4a72ad7053479de0
goobygoofer <117778445+goobygoofer@users.noreply.github.com>
2023-04-06 08:16:02 -0500

    Add files via upload

--- base_tiles.js
+++ base_tiles.js
@@ -31,247 +31,208 @@ baseTiles = {//redo entirely and in order
     "stump2":[64,1120],
     "stump3":[80,1120],
     "campfire":[32,384],//
+    "rain":[144,432],//
+  }
+
+  npcTiles = {
+    "skelR":[128,16],
+    "skelL":[128,32]
   }
 
   gameObjects = {
+    "skeleton":{//character.sprite is now both sets of coords for left and right facing
+        "name":"skeleton",//so player/npc sprites get drawn differently
+        "id":null,//add id upon npc object creation (if player id added, it will work differently than here)
+                  //this way just this object gets put on tile and actual npc object is referenced by id here
+        "sprite":{"left":npcTiles['skelR'],"right":npcTiles['skelL']},
+        "facing":"left",//players and npcs need this
+        "type":"npc",
+        "collision":true
+    },
+    "rain":{//this is also the speedboot trail sprite
+        "name":"rain",
+        "sprite":baseTiles['rain'],
+        "type":"object",
+        "collision":false,
+    },
     "stump3":{
         "name":"stump3",
         "sprite":baseTiles['stump3'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "stump2":{
         "name":"stump2",
         "sprite":baseTiles['stump2'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "stump1":{
         "name":"stump1",
         "sprite":baseTiles['stump1'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "campfire":{
         "name":"campfire",
         "sprite":baseTiles['campfire'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "grave":{
         "name":"grave",
         "sprite":baseTiles['grave'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "stoneblock":{
         "name":"stoneblock",
         "sprite":baseTiles['stoneblock'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "woodblock":{
         "name":"woodblock",
         "sprite":baseTiles['woodblock'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "rock":{
         "name":"rock",//won't be any issues with this? will probably need id and stuff eventually
         "sprite":baseTiles['rock'],//object['sprite'][0]/[1] are sprite coords
         "type":"object",//as opposed to player or base-tile
         "collision":true,
-        "x":null,//this might be unnecessary but we'll see
-        "y":null
     },
     "grass":{
         "name":"grass",
         "sprite":baseTiles['grass'],
         "type":"base-tile",//as opposed to object or player
         "collision":false,
-        "x":null,
-        "y":null
     },
     "tree":{
         "name":"tree",
         "sprite":baseTiles['tree'],//need to account for a chopped up tree (same obj, change sprite?)
         "type":"object",//as opposed to object or player
         "collision":true,
-        "x":null,
-        "y":null
     },
     "water":{
         "name":"water",
         "sprite":baseTiles['water'],
         "type":"base-tile",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "oak":{
         "name":"oak",
         "sprite":baseTiles['oak'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "snow":{
         "name":"snow",
         "sprite":baseTiles['snow'],
         "type":"base-tile",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "mushroom":{
         "name":"mushroom",
         "sprite":baseTiles['mushroom'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "deadtree":{
         "name":"deadtree",
         "sprite":baseTiles['deadtree'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "snowtree":{
         "name":"snowtree",
         "sprite":baseTiles['snowtree'],
         "type":"object",
         "collision":true,
-        "x":null,
-        "y":null
     },
     "flower1":{
         "name":"flower1",
         "sprite":baseTiles['flower1'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "flower2":{
         "name":"flower2",
         "sprite":baseTiles['flower2'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "flower3":{
         "name":"flower3",
         "sprite":baseTiles['flower3'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathHORIZ":{
         "name":"pathHORIZ",
         "sprite":baseTiles['pathHORIZ'],
         "type":"object",//ok so more than one object per tile?
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathVERT":{
         "name":"pathVERT",
         "sprite":baseTiles['pathVERT'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathTDWN":{
         "name":"pathTDWN",
         "sprite":baseTiles['pathTDWN'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathTUP":{
         "name":"pathTUP",
         "sprite":baseTiles['pathTUP'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathTRT":{
         "name":"pathTRT",
         "sprite":baseTiles['pathTRT'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathTLT":{
         "name":"pathTLT",
         "sprite":baseTiles['pathTLT'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathCRS":{
         "name":"pathCRS",
         "sprite":baseTiles['pathCRS'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathCRV1":{
         "name":"pathCRV1",
         "sprite":baseTiles['pathCRV1'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathCRV2":{
         "name":"pathCRV2",
         "sprite":baseTiles['pathCRV2'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathCRV3":{
         "name":"pathCRV3",
         "sprite":baseTiles['pathCRV3'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     },
     "pathCRV4":{
         "name":"pathCRV4",
         "sprite":baseTiles['pathCRV4'],
         "type":"object",
         "collision":false,
-        "x":null,
-        "y":null
     }
 }
--- client.js
+++ client.js
@@ -7,11 +7,20 @@ function toggleCollision(){
     master_collision=false;
   }
 }
+var raining = true;
+function toggleRain(){
+  if (raining===true){
+    raining=false;
+  } else{
+    raining=true;
+  }
+}
 
 //UI setup////////////////////////////////////////////////////////////////////////////
 //main canvas element
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+//ctx.globalAlpha=0.5;
 //to display example tile next to dropdown menu
 const sprtCanvas = document.getElementById('spriteCanvas');
 const sprtCtx = sprtCanvas.getContext('2d');
@@ -26,7 +35,12 @@ if (myData!==null){
   tile_map=JSON.parse(myData);
 }
 //save map to local storage
-function saveToLocal(){
+function saveToLocal(){//now only runs if player clicks button
+  for (row in tile_map){
+    for (col in tile_map[row]){
+      tile_map[row][col].objects=filterObjByKeyVal(tile_map[row][col].objects, "type", "npc");
+    }
+  }
   localStorage.setItem("userMap", JSON.stringify(tile_map))
 }
 //delete map from local storage
@@ -37,7 +51,7 @@ function clearUserMap (){
 
 //setup dropdown menu for base tiles
 const dropdown = document.getElementById('tile-dropdown');
-console.log(dropdown)
+//console.log(dropdown)
 
 for (const key in gameObjects){//change to key in objects
   if (gameObjects.hasOwnProperty(key)){
@@ -49,7 +63,7 @@ for (const key in gameObjects){//change to key in objects
 
 dropdown.addEventListener("change",function(){
   const selectedTile = this.value;
-  console.log(selectedTile)
+  //console.log(selectedTile)
   objectToPlace=gameObjects[selectedTile];
   sprtCtx.clearRect(0,0,16,16);
   sprtCtx.drawImage(spriteSheet, baseTiles[selectedTile][0],baseTiles[selectedTile][1], 16,16,
@@ -93,8 +107,10 @@ document.getElementById('downButton').addEventListener('click', () => movePlayer
 document.getElementById('rightButton').addEventListener('click', () => movePlayer('right'));
 document.getElementById('placeButton').addEventListener('click', () => placeTile(objectToPlace));
 document.getElementById('resetButton').addEventListener('click', () => clearUserMap());
+document.getElementById('saveButton').addEventListener('click', () => saveToLocal());
 document.getElementById('resetTileButton').addEventListener('click', () => resetTile());
 document.getElementById('collisionButton').addEventListener('click', () => toggleCollision());
+document.getElementById('rainButton').addEventListener('click', () => toggleRain());
 //end UI setup//////////////////////////////////////////////////////////////////////////////////////
 
 //set up ghost player///////////////////////////////////////////////////////////////////////////////
@@ -132,6 +148,11 @@ function interactNext(nextX, nextY){
   if (master_collision===true){
     return false
   }
+  check_collision=checkCollision(nextX, nextY);
+  return check_collision;
+}
+
+function checkCollision(nextX, nextY){
   if (tile_map[nextX][nextY]['sprite']['collision']===true){
     return true
   } else {
@@ -146,6 +167,7 @@ function interactNext(nextX, nextY){
     }
   }
 }
+
 //instead check players next potential tile in tile_map
 function movePlayer(direction) {
   let potentialX=playerX;
@@ -197,7 +219,7 @@ function placeTile (objToPlace){
   if (objToPlace.type==='base-tile'){
     tile_map[playerX][playerY]['sprite']=objectToPlace;
   }
-  saveToLocal();
+  //saveToLocal();//make button for this
 }
 
 function resetTile(){
@@ -207,7 +229,90 @@ function resetTile(){
 
 //end player setup////////////////////////////////////////////////////////////////////
 
+//passive npc setup testing
+move_dir=["up","down","left","right"];
+
+function moveNPC(curX,curY){//for passive (non-aggressive/tracking) movement
+  //calculate random move, return x,y
+  let potentialX=null;
+  let potentialY=null;
+  let facing=null;
+  moveDir = move_dir[Math.floor(Math.random() * 4)];
+  if (moveDir==='up'){potentialX=curX-1;potentialY=curY;};
+  if (moveDir==='down'){potentialX=curX+1;potentialY=curY;};
+  if (moveDir==='left'){
+    potentialX=curX;potentialY=curY-1;
+    facing="left";
+  };
+  if (moveDir==='right'){
+    potentialX=curX;potentialY=curY+1;
+    facing="right";
+  };
+  //check potential tile for collision
+  let collision = checkCollision(potentialX, potentialY);
+  if (collision){return [curX, curY, facing]}
+  else {
+    return [potentialX, potentialY, facing];
+  }
+}
+
+function filterObjById(arr, id){//returns list with id del'd
+  return arr.filter(obj => !obj.hasOwnProperty('id') && obj.id !==id);
+}
+
+function filterObjByKeyVal(arr, key, val){
+  return arr.filter(obj => obj.hasOwnProperty(key) && obj[key] !== val);
+}
+
+var npcs=[];//npc's in game, {"[id]":npc_object}
+function skeleton(){
+  //going to need id eventually?
+  this.spriteData=gameObjects['skeleton'];
+  this.spriteData.id='1234';//just testing, figure out way to generate
+  this.x=null;//don't need these because in spriteData?
+  this.y=null;
+  this.lastTime=Date.now();//timestamp
+  this.update = function(){
+    let now = Date.now();
+    if (now > this.lastTime+2000){
+      this.lastTime=now;
+      let newCoords=moveNPC(this.x,this.y);//returns x,y
+      //remove self from current tile
+      if (newCoords[2]!==null){
+        this.spriteData.facing=newCoords[2];
+      }
+      tile_map[this.x][this.y].objects = filterObjById(tile_map[this.x][this.y].objects, this.spriteData.id);
+      this.x=newCoords[0];
+      this.y=newCoords[1];
+      //add self to new tile
+      tile_map[this.x][this.y].objects.push(this.spriteData)
+    }
+  }
+}
+//skeleton testing
+var skeleton_1 = new skeleton();
+skeleton_1.x=17;
+skeleton_1.y=17;
+tile_map[skeleton_1.x][skeleton_1.y].objects.push(skeleton_1.spriteData);
+npcs.push(skeleton_1);
+//npcs[skeleton_1.id]=skeleton_1;
+
 //main
+function drawWeather(x,y){
+  //for now just draw rain on random tiles
+  if (raining===true){
+    let randomInt = Math.random();
+    if (randomInt>=0.9){
+      ctx.drawImage(spriteSheet, gameObjects['rain']['sprite'][0],gameObjects['rain']['sprite'][1], 16,16, 
+        x,y, 16,16)
+    }
+  }
+}
+
+const gradient = ctx.createRadialGradient(canvas.width/2, canvas.height/2, 10, canvas.width/2, canvas.height/2, canvas.width/1.5);
+gradient.addColorStop(0, "transparent");
+gradient.addColorStop(1, "gray");
+
 function drawMap(disp_area){
   for (row in disp_area){
     //draw base sprite from that area of tile_map
@@ -217,22 +322,48 @@ function drawMap(disp_area){
       let sprtLoc=gameObjects[tile_map[sprtX][sprtY]['sprite']['name']]['sprite']
       ctx.drawImage(spriteSheet, sprtLoc[0],sprtLoc[1], 16,16,
         col*BLOCKSIZE, row*BLOCKSIZE, 16,16);
-      //for objects on current tile, draw them in logical order
+      //draw weather
+      drawWeather(col*BLOCKSIZE,row*BLOCKSIZE);
+      //drawNPCs(disp_area);//function to draw npcs in player view
       if (tile_map[sprtX][sprtY]['objects'].length!=0){
+        //check type.object (for now is only object or npc)
         for (object in tile_map[sprtX][sprtY]['objects']){
-          sprtLoc=tile_map[sprtX][sprtY]['objects'][object]['sprite']
-          ctx.drawImage(spriteSheet, sprtLoc[0],sprtLoc[1], 16,16,
-            col*BLOCKSIZE, row*BLOCKSIZE, 16,16);
+          //draw objects
+          if (tile_map[sprtX][sprtY]['objects'][object].type==='object'){
+            sprtLoc=tile_map[sprtX][sprtY]['objects'][object]['sprite']
+            ctx.drawImage(spriteSheet, sprtLoc[0],sprtLoc[1], 16,16,
+              col*BLOCKSIZE, row*BLOCKSIZE, 16,16);
+          }
+          //draw npcs
+          else if (tile_map[sprtX][sprtY]['objects'][object].type==='npc'){
+            let npcFacing=tile_map[sprtX][sprtY]['objects'][object].facing;
+            sprtLoc=tile_map[sprtX][sprtY]['objects'][object]['sprite'][npcFacing];
+            ctx.drawImage(spriteSheet, sprtLoc[0],sprtLoc[1], 16,16,
+              col*BLOCKSIZE, row*BLOCKSIZE, 16,16);
+          }
         }
       }
     }
-  }  
+  }
+  if (raining){//gray gradient that makes it look dark and cloudy!
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+  }
+}
+
+function updateNPCs(){
+  //for npc in npcs, npcs[npc].update();
+  for (npc in npcs){
+    npcs[npc].update();
+  }
 }
 
 function update(){
   //game logic here
   ctx.clearRect(0,0,300,300);
   let disp_area=getDispArea();
+  //uddate player
+  updateNPCs();
   drawMap(disp_area);
   drawPlayer();//or draw player as object of tile they are on?
 }
